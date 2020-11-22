786a35ef1a3d9b6d7f365e723bb4b4bd92ca91a5
Gian Hervera <ghervera@academic.rrc.ca>
2020-11-22 01:24:14 -0600

    Products attachments included

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   has_many :order
   validates :product, :type_name, :company, :inches, :screen_resolution,
             :CPU, :RAM, :memory, :GPU, :price, presence: true
+  has_one_attached :image
 end
