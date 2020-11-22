59620e057518fc05a15dcab43b709b92d66f4469
Gian Hervera <ghervera@academic.rrc.ca>
2020-11-22 01:35:26 -0600

    Image adding has been included in the active admin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,5 +1,21 @@
 ActiveAdmin.register Product do
-
 permit_params :product_id, :company, :product, :type_name, :inches, :screen_resolution, :CPU, :RAM, :memory, :GPU, :images, :price
+form do |f|
+  f.semantic_errors
+  f.inputs do
+    f.input :company
+    f.input :product
+    f.input :type_name
+    f.input :inches
+    f.input :screen_resolution
+    f.input :CPU
+    f.input :RAM
+    f.input :memory
+    f.input :GPU
+    f.input :price
+    f.input :images, as: :file, hint: f.object.image.present? ? image_tag(f.object.image) : ""
+  end
+  f.actions
+end
 
 end
