23efeddf6b6547fc9a0a4191cf0fa7b962908baf
ghervera <59581339+ghervera@users.noreply.github.com>
2020-11-29 17:40:44 -0600

    added changes to css

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,20 @@
 class SearchesController < ApplicationController
+   def show
+    @search = Search.find(params[:id])
+  end
+
+  def new
+      @search = Search.new
+    # @categories = Product.distinct.pluck(category_url)
+    end
+    def create
+      @search = Search.create(search_params)
+      redirect_to @search
+    end
+
+    private
+
+    def search_params
+      params.require(:search).permit(:keywords, :category, :min_price, :max_price)
+    end
 end
--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,11 @@
 class Search < ApplicationRecord
+  def search_products
+    products = Product.all
+
+    products = products.where(["company LIKE ?","%#{keywords}%"]) if keywords.present?
+    products = products.where(["price >= LIKE ?","%#{min_price}%"]) if min_price.present?
+    products = products.where(["price <= LIKE ?","%#{max_price}%"]) if max_price.present?
+
+    return products
+  end
 end
