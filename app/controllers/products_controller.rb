5e583ed6f34ed268f6a6e70746335248f215e512
ghervera <59581339+ghervera@users.noreply.github.com>
2020-12-07 18:20:58 -0600

    Added breadcrumbs to the site

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,17 @@
 class ProductsController < ApplicationController
-
+  before_action :add_index_breadcrumb, only: [:show, :new, :edit]
   def index
   @pagy, @products = pagy(Product.search(params[:search]))
-
+    add_breadcrumb('Products')
   end
 
   def show
     @product = Product.find(params[:id])
+    add_breadcrumb(@product.product)
   end
+
+  def add_index_breadcrumb
+    add_breadcrumb('Products', products_path)
+  end
+
 end
